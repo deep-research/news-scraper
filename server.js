10dbbcc3708032ee7eb91a87df41b4cc31154b6e
Victor <deep-research>
2018-01-27 14:42:08 -0600

    forgot port var

--- server.js
+++ server.js
@@ -224,7 +224,8 @@ app.get("/*", function(req, res) {
 })
 
 // Listen on port 3000
-app.listen(3000, function() {
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
     console.log("App running on port 3000!");
 });
   
